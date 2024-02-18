92711419f38e53478e287266c861166d631feced
fathi-fayez <fathifayez155@gmail.com>
2024-02-19 01:43:33 +0200

    and small edit

--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -71,6 +71,8 @@ li {
     background-color: #f1f1f1;
     box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
         0 8px 10px -6px rgb(0 0 0 / 0.1);
+    width: 75%;
+    margin: auto;
 
     &:hover {
         .todo-actions {
